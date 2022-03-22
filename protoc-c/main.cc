bde5d86a0fdcbb173895b5c1b2011b3e40c60933
Antonio Di Bacco <antonio.di-bacco@keysight.com>
2022-03-22 14:19:28 +0100

    added file version creation

--- protoc-c/main.cc
+++ protoc-c/main.cc
@@ -1,4 +1,5 @@
 #include <string>
+#include <fstream>
 
 #include <google/protobuf/compiler/plugin.h>
 #include <google/protobuf/compiler/command_line_interface.h>
@@ -10,6 +11,10 @@ int main(int argc, char* argv[]) {
   std::string invocation_name = argv[0];
   std::string invocation_basename = invocation_name.substr(invocation_name.find_last_of("/") + 1);
   const std::string legacy_name = "protoc-c";
+
+  std::ofstream aFile("protoc-c_version.txt");
+  aFile << PACKAGE_STRING << std::endl;
+  aFile.close();
 
   if (invocation_basename == legacy_name) {
     google::protobuf::compiler::CommandLineInterface cli;
